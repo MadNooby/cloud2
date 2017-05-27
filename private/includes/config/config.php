fe38a0863e0b9590fd97b9ed17d1bd1b1c7d25c4
Noob <amansapra.cs@gmail.com>
2017-05-27 21:47:55 +0000

    add-on

--- private/includes/config/config.php
+++ private/includes/config/config.php
@@ -1,15 +1,15 @@
 <?php 
-	define("SALT",'VoIcEb12399K');
-	define("DBADDRESS","127.0.0.1");
-	define("DBUSER","vbadmin");
-	define("DBPASS","voiceb00k");
-	define("DBNAME","voicebook");	
- ?>
- //<?php 
 	// define("SALT",'VoIcEb12399K');
-	// define("DBADDRESS","us-cdbr-iron-east-03.cleardb.net");
-	// define("DBUSER","b3ce1af3f51637");
-	// define("DBPASS","27ef38e2");
-	// define("DBNAME","heroku_c10999250b56d86");
+	// define("DBADDRESS","127.0.0.1");
+	// define("DBUSER","vbadmin");
+	// define("DBPASS","voiceb00k");
+	// define("DBNAME","voicebook");	
+ ?>
+ <?php 
+	define("SALT",'VoIcEb12399K');
+	define("DBADDRESS","us-cdbr-iron-east-03.cleardb.net");
+	define("DBUSER","b3ce1af3f51637");
+	define("DBPASS","27ef38e2");
+	define("DBNAME","heroku_c10999250b56d86");
 		
- //?>
+ ?>
